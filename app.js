33e0785ea76175d52ce62f26a191f6a726113933
Sonam Chaudhary <sonamchaudhary@Sonams-MacBook-Air.local>
2023-12-29 13:56:44 +0530

    we can or cannot use an array for siblings and child

--- app.js
+++ app.js
@@ -45,18 +45,18 @@ console.log(heading);
     </div>
 </div> */}
 
-const parent = React.createElement("div",{id:"parent"},[
-    React.createElement("div",{id:"child"},[
+const parent = React.createElement("div",{id:"parent"},
+    React.createElement("div",{id:"child"},
         React.createElement("h1",{},"I'm h1 tag"),
         React.createElement("h2",{},"I'm h2 tag")
-    ]
+    
     ),
-    React.createElement("div",{id:"child2"},[
+    React.createElement("div",{id:"child2"},
         React.createElement("h1",{},"I'm h1 tag"),
         React.createElement("h2",{},"I'm h2 tag")
-    ]
+    
     )
-]
+
 );
 
 
