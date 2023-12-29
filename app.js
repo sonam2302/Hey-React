c13bf267961e5e3640bbcc1dffe7cc6475ed8895
Sonam Chaudhary <sonamchaudhary@Sonams-MacBook-Air.local>
2023-12-29 12:34:29 +0530

    fix one problem

--- app.js
+++ app.js
@@ -12,6 +12,28 @@ console.log(heading);
 // xy:"abc"
 // [[Prototype]]: Object
 
+
+
+// problem 2 - create siblings: -
+//     <div id="parent">
+//         <div id="child">
+//             <h1>I'm h1 tag</h1>
+//             <h2>I'm h2 tag</h2>
+//         </div>
+//     </div>
+// sol - to make siblings in 3rd arg u have to pass 2 children in an array
+
+const parent = React.createElement("div",{id:"parent"},
+React.createElement("div",{id:"child"},[
+    React.createElement("h1",{},"I'm an h1 tag"),
+    React.createElement("h2",{},"I'm an h2 tag"),
+
+]
+)
+)
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+// root.render(heading);
+root.render(parent);
+
 // this render method converting this object to h1 tag and placing it in root node 
